3f90221a1e7679dea1b957eb46bb236b7f390cd5
ccookcomp <ccook19@binghamton.edu>
2017-05-03 17:40:43 -0400

    Add files via upload

---
+++ SuperBlock.cpp
@@ -0,0 +1,10 @@
+#include "SuperBlock.h"
+
+SuperBlock :: SuperBlock(int numB1,int BS1,int freeB1, int freeIn1){
+		numB=numB1;
+		BS=BS1;
+		freeB=freeB1;
+		freeIn=freeIn1;
+
+	}
+
---
+++ SuperBlock.h
@@ -0,0 +1,21 @@
+#include <string>
+
+
+class SuperBlock{
+	public:
+		int numB;
+		int BS;
+		int freeB;
+		int freeIn;
+		int off;
+		int inode;
+		SuperBlock(int numB1,int BS1,int freeB1, int freeIn1);
+		/*SuperBlock(int numB1,int BS1,int freeB1, int freeIn1){
+		numB=numB1;
+		BS=BS1;
+		freeB=freeB1;
+		freeIn=freeIn1;
+
+	}*/
+};
+
