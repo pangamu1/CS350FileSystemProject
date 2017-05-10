ea153a0d0374deaf0b804d5e29fa62395fe0198d
SteveLunes <sliouni1@binghamton.edu>
2017-05-10 13:08:26 -0400

    ssfs.h

--- ssfs.h
+++ ssfs.h
@@ -2,17 +2,27 @@
 #define _SSFS_H_
 
 #include <fstream>
-#include <list>
+//#include <list>
 #include <map>
 #include <string>
 #include <vector>
+#include <pthread.h>
+#include <semaphore.h>
 
+using namespace std;
+pthread_t tid, tid1;
+pthread_attr_t attr;
+sem_t full, empty;
+pthread_mutex_t mu;
+
+vector <vector<string>> buffer(1);
 
 class ssfs{
 public:
 	ssfs(std::vector<std::string> args, int file_size, int block_size, int direct_blocks);
 	map <string, int> inodeMap;
 	map <string, int>::iterator it;
+	int file_size, block_size, direct_blocks;
 	void create(std::vector<std::string> args,SuperBlock sb);
 	void import(std::vector<std::string> args,SuperBlock sb, bool*freeB);
 	void cat(std::vector<std::string> args,SuperBlock sb);
@@ -26,4 +36,23 @@ public:
 	//std::map <std::string, int> inodeMap;
 };
 
+struct argies{
+	SuperBlock s;
+	bool *freeB;
+	string filename;
+	ssfs *fs;
+	argies(){}
+	argies(SuperBlock sb, bool *f, string fn, ssfs *sfs){
+		s = sb;
+		freeB = f;	
+		filename = fn;
+		fs = sfs;
+	}
+	argies(argies *argy) {
+		s = argy->s;
+		freeB = argy->freeB;
+		filename = argy->filename;
+		fs = argy->fs;
+	}
+};
 #endif
